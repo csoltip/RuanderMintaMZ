adff08aa417381a7806872a9e6c3f4e5618137bb
CsoltiP <csolti@szamalk-szalezi.hu>
2021-04-12 17:51:12 +0200

    üres, csak feladatleírások

---
+++ MintaMZ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintaMZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //fájl beolvasás
+
+            //adatok száma
+
+            //legtöbb vmi
+
+            //van e egy valamilyen
+
+            //mindenki valamiylen e
+
+            //hányféle van
+
+            //melyikből mennyi van
+
+            //fájlkiírás
+        }
+    }
+}
