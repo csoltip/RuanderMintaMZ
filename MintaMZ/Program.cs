611ad00e994dc03b3e6f90cefc0e2e26f14c05e5
CsoltiP <csolti@szamalk-szalezi.hu>
2021-04-12 18:05:57 +0200

    szélsőérték

--- MintaMZ/Program.cs
+++ MintaMZ/Program.cs
@@ -9,15 +9,41 @@ namespace MintaMZ
 {
     class Program
     {
+        class Auto
+        {
+            public int ertek;
+        }
+
         static void Main(string[] args)
         {
             //fájl beolvasás
             string[] sorok = File.ReadAllLines("forras.csv");
+            List<Auto> autok = new List<Auto>();
+            foreach (string sor in sorok.Skip(1))//skip, ha van fejléc
+            {
+                autok.Add(new Auto());
+            }
+
+            /* innen csak az autok listával foglalkozunk */
 
             //adatok száma
-            int N = sorok.Length;//ha van fejléc, akkor - 1
+            int N = autok.Count;
 
-            //legtöbb vmi
+            //legvalamilyenebb
+            /* szélsőérték keresés */
+            int minIndex = 0, maxIndex = 0;
+            for (int i = 1; i < N; i++)
+            {
+                if(autok[i].ertek > autok[maxIndex].ertek)
+                {
+                    maxIndex = i;
+                }
+                if (autok[i].ertek < autok[minIndex].ertek)
+                {
+                    minIndex = i;
+                }
+            }
+            Console.WriteLine($"a legnagyobb: {autok[maxIndex]}, a legkisbb: {autok[minIndex]}");
 
             //van e egy valamilyen
 
