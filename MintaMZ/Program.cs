9e4a7c55962a99205eb539b5340d3c4f7976cd5c
CsoltiP <csolti@szamalk-szalezi.hu>
2021-04-12 17:55:18 +0200

    fájlbeolvasás

--- MintaMZ/Program.cs
+++ MintaMZ/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace MintaMZ
         static void Main(string[] args)
         {
             //fájl beolvasás
+            File.ReadAllLines("forras.csv");
+
 
             //adatok száma
 
